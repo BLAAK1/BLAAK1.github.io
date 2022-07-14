42c902e337df50df03096522356d7b336e78b632
BLAAK1 <zegaroskar@gmail.com>
2022-07-14 02:27:38 +0200

    Summary

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-function menu() {}
+"use strict";
+// Burger menu
+
+function burgerMenu() {
+  if (menu.style.display === "none") {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
+}
+//Slider
